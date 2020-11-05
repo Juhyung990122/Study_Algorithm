72a4e1c29523d678410f7898a7406195ef2eab84
Juhyung990122 <harukawa99@naver.com>
2020-11-05 19:21:36 +0900

    [Greedy]숫자 카드 게임

---
+++ Greedy/Greedy2.py
@@ -0,0 +1,10 @@
+#Greedy 2 숫자 카드 게임
+n,m = map(int,input().split())
+answer = 0임
+
+for i in range(n):
+    card = list(map(int,input().split()))
+    min_value = min(card)
+    answer = max(answer, min_value)
+
+print(answer)
