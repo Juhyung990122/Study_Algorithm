32cef409f18f0e45f10deb8ee4630ba22d9d4506
Juhyung990122 <harukawa99@naver.com>
2020-11-05 19:07:48 +0900

    [Greedy] 큰수의 법칙(improve)

--- Greedy/Greedy1.py
+++ Greedy/Greedy1.py
@@ -21,3 +21,22 @@ while True:
     m -= 1
 
 print(answer)
+
+# 개선안
+n,m,k = map(int,input().split())
+numlist = list(map(int,input().split()))
+
+numlist.sort()
+
+first = numlist[n-1]
+second = numlist[n-2]
+
+answer = 0
+count = m//(k+1) * k + m % (k+1)
+
+answer = count * first
+answer +=  (m - count) * second
+
+print(answer)
+
+
