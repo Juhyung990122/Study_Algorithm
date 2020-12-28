02ed59b0372afcb455846b066bae83fd9f5ce6a1
Juhyung990122 <harukawa99@naver.com>
2020-12-28 20:56:45 +0900

    [BinarySearch]부품찿기

---
+++ Binary Search/BinarySearch1.py
@@ -0,0 +1,39 @@
+#부품찾기
+def Binary(start,end,target):
+    if start <= end:
+        mid = (start + end) // 2
+        if bupum_list[mid] == target:
+            return mid
+        elif bupum_list[mid] < target:
+            return Binary(mid+1,end,target)
+        else:
+            return Binary(start,mid-1,target)
+    return None
+
+
+def Binary2(start,end,target):
+    while start<=end:
+        mid = (start + end) // 2
+        if bupum_list[mid] == target:
+            return mid
+        elif bupum_list[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return None
+
+n = int(input())
+bupum_list = list(map(int,input().split(' ')))
+m = int(input())
+req_list = list(map(int,input().split(' ')))
+
+
+bupum_list=sorted(bupum_list)
+req_list = sorted(req_list)
+
+for i in req_list:
+    result = Binary(0,n-1,i)
+    if result != None:
+        print('yes',end=' ')
+    else:
+        print('no',end=' ')
