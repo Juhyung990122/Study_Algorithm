9e7e3d0426b8d0550cd45b2a530cae7e5fa37265
Juhyung990122 <harukawa99@naver.com>
2021-02-23 00:11:46 +0900

    [BOJ]좌표 정렬하기 시간단축

--- exercise/좌표 정렬하기.py
+++ exercise/좌표 정렬하기.py
@@ -1,8 +1,9 @@
-n = int(input())
+import sys
+
 num = list()
 
-for _ in range(n):
-    x,y = list(map(int,input().split()))
+for _ in range(int(sys.stdin.readline().rstrip())):
+    x,y = map(int, sys.stdin.readline().rstrip().split())
     num.append([x,y])
 
 #비교할 아이템의 요소가 복수 개일 경우, 튜플로 그 순서를 내보내주면 된다.
