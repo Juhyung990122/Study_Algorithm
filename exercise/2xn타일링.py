db774cb6386bdcbd1f14df6726417381e6412ded
Juhyung990122 <harukawa99@naver.com>
2021-04-01 17:37:23 +0900

    [BOJ]2xn타일링,계단오르기(복습)

--- exercise/2xn타일링.py
+++ exercise/2xn타일링.py
@@ -1,5 +1,5 @@
 import sys
-
+'''
 n = int(sys.stdin.readline())
 #dp에는 방법 수 저장
 dp = [0] * 1001
@@ -14,3 +14,18 @@ for i in range(2,n+1):
     dp[i] = (dp[i-2] + dp[i-1]) % 10007
 
 print(dp[n])
+'''
+
+def solution(n):
+    answer = 0
+    dp = [0]*n
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2,n):
+        dp[i] =(dp[i-2] + dp[i-1]) % 1000000007
+    
+    answer = dp[-1] 
+    return answer
+
+print(solution(10))
--- exercise/계단오르기.py
+++ exercise/계단오르기.py
@@ -1,4 +1,4 @@
-n = int(input())
+'''n = int(input())
 stairs = list()
 for _ in range(n):
     stairs.append(int(input()))
@@ -14,4 +14,20 @@ for i in range(3,n):
     dp.append(max(stairs[i] + stairs[i-1] + dp[i-3], stairs[i] + dp[i-2]))
 
 print(dp[-1])
-    
+    '''
+
+import sys
+n = int(sys.stdin.readline())
+cost = [0] * 300
+for i in range(n):
+    cost[i] = int(sys.stdin.readline())
+#n번째 계단으로 가는 방법 두개(1칸 or 2칸) 중 가장 cost가 큰 애들 저장
+dp =  [0] * 300
+
+dp[0] = cost[0]
+dp[1] = cost[0]+cost[1]
+dp[2] = max(cost[2]+cost[1],cost[0]+cost[2])
+        
+for i in range(3,n):
+    dp[i] = max(cost[i]+cost[i-1]+dp[i-3], cost[i]+dp[i-2])
+print(dp[n-1])
