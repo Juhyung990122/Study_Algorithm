071060ba0fb78ac3aa2c1c7e2a744753d84dd873
Juhyung990122 <harukawa99@naver.com>
2021-04-08 22:24:12 +0900

    [Programmers,BOJ]단속카메라, 바이러스(re)

--- exercise/바이러스.py
+++ exercise/바이러스.py
@@ -1,5 +1,5 @@
+'''
 import sys
-
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 visited = [0] * n
@@ -26,3 +26,34 @@ while queue:
         
 
 print(answer)
+'''
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+graph = dict()
+for init in range(1,n+1):
+    graph[init] = list()
+
+for _ in range(m):
+    c1,c2 = map(int,sys.stdin.readline().split())
+    graph[c1].append(c2)
+    graph[c2].append(c1)
+    
+def bfs(start):
+    answer = 0
+    queue = deque()
+    queue.append(start)
+    visited = [1]+[0] * n
+    visited[start] = 1
+    while queue:
+        now = queue.popleft()
+        for i in range(len(graph[now])):
+            a_now = graph[now][i]
+            if visited[a_now] == 0:
+                queue.append(a_now)
+                visited[a_now] = 1
+                answer += 1
+    return answer
+
+print(bfs(1))
