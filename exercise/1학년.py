14a5f7b3c18a9cb368c08810603f56c5d3290046
Juhyung990122 <harukawa99@naver.com>
2021-04-17 23:59:25 +0900

    [BOJ]1학년

--- exercise/1학년.py
+++ exercise/1학년.py
@@ -3,3 +3,17 @@ import sys
 n = int(sys.stdin.readline())
 num_list = list(map(int,sys.stdin.readline().split()))
 
+dp = [[0] * 20 for _ in range(n)]
+dp[0][num_list[0]] = 1
+
+for i in range(1,n):
+    for j in range(20):
+        if j - num_list[i] >= 0:
+            if dp[i-1][j - num_list[i]] != 0:
+                dp[i][j] += dp[i-1][j]
+        if j + num_list[i] < 20:
+            if dp[i-1][j + num_list[i]] != 0:
+                dp[i][j] += dp[-1][j]
+print(dp)
+
+
---
+++ exercise/EX.java
@@ -0,0 +1,19 @@
+package algorithm.exercise;
+import java.math.BigInteger;
+import java.util.Arrays;
+public class EX{
+    public static void main(String [] args ){
+        String s = Solution("1234","5678");
+        System.out.print(s);
+    }
+
+    private static String Solution(String a, String b) {
+        String answer = "6912";
+        BigInteger aINT = new BigInteger(a);
+        BigInteger bINT = new BigInteger(b);
+        BigInteger sum = aINT.add(bINT);
+        answer = (String)sum.toString();
+        return answer;
+
+    }
+}
