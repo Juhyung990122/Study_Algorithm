7c8d7a3fa1791360046998014cad083d84f67d53
Juhyung990122 <harukawa99@naver.com>
2021-04-18 22:03:24 +0900

    [BOJ]평범한 배냥,동전2,연속합

--- exercise/동전2.py
+++ exercise/동전2.py
@@ -1,9 +1,4 @@
-import sys
-n,k = map(int,sys.stdin.readline().split())
-coin = list()
-for _ in range(n):
-    coin.append(int(sys.stdin.readline().rstrip()))
-
+'''
 dp = [0]+[10001] * k
 
 for i in range(n):
@@ -11,6 +6,25 @@ for i in range(n):
         dp[j] = min(dp[j],dp[j-coin[i]]+1)
 
 if dp[-1] == 10001:
+    print(-1)
+else:
+    print(dp[-1])
+    '''
+import sys
+n,k = map(int,sys.stdin.readline().split())
+coin = list()
+for _ in range(n):
+    coin.append(int(sys.stdin.readline().rstrip()))
+coin.sort()
+
+dp = [100001] * (k+1)
+
+for i in range(n):
+    for j in range(k+1):
+            dp[j] = min(dp[j],dp[j - coin[i]]+1)
+    print(dp)
+
+if dp[-1] == 100001:
     print(-1)
 else:
     print(dp[-1])
---
+++ exercise/평범한 배낭.py
@@ -0,0 +1,19 @@
+import sys
+
+n,k = map(int,sys.stdin.readline().split())
+obj_list = list()
+
+for _ in range(n):
+    obj_list.append(list(map(int,sys.stdin.readline().split())))
+
+dp = [[0] * (k+1) for _ in range(n)]
+# 가치합 = i번째 물건을 넣었을때 최대 가치
+for i in range(len(obj_list)):
+    w,v = obj_list[i]
+    for j in range(k+1):
+        print(w,j)
+        if w <= j:
+            dp[i][j] = max(v + dp[i-1][j - w],dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(max(dp[-1]))
