8ff70125dcf26e215bed154353580b0443b7f980
Juhyung990122 <harukawa99@naver.com>
2021-05-03 18:33:57 +0900

    [BOJ]토마토, 단지번호붙이기

---
+++ exercise/BOJ14502.java
@@ -0,0 +1,87 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+
+public class BOJ14502 {
+    private static int x,y,answer;
+    private static int[][] graph;
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws IOException {
+        //// 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        x = Integer.parseInt(input[0]);
+        y = Integer.parseInt(input[1]);
+        graph = new int[y][x];
+        
+        for (int i = 0; i < y; i++){
+            input = br.readLine().split(" ");
+            for(int j = 0; j < x; j++){
+                graph[i][j] = Integer.parseInt(input[j]);  
+            }
+        }
+       //// 
+        bfs(x,y,graph);
+
+        ///답출력
+        answer = 0;
+        for (int i = 0; i < y; i++){
+            for(int j = 0; j < x; j++){
+                if(graph[i][j] == 0){
+                    System.out.println(-1);
+                    return;
+                }
+                answer = Math.max(answer, graph[i][j]);
+            }
+        }
+        System.out.println(answer-1);
+    }
+
+    public static void bfs(int x, int y, int[][] graph) {
+        Queue<Node> queue = new LinkedList<Node>();
+
+        for(int i = 0; i < y; i++){
+            for(int j = 0; j < x; j++){
+                if(graph[i][j] == 1){
+                    queue.add(new Node(j,i));
+                }
+            }
+        }
+
+        while(!queue.isEmpty()){
+            Node now = queue.poll();
+            for(int i = 0; i < 4; i++){
+                int moveX = now.x + dx[i];
+                int moveY = now.y + dy[i];
+
+                if(0 <= moveX && moveX < x && 0 <= moveY && moveY < y ){
+                    if(graph[moveY][moveX] == 0){
+                        graph[moveY][moveX] = graph[now.y][now.x] + 1;
+                        queue.add(new Node(moveX,moveY));
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Node{
+    int x;
+    int y;
+
+    Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
--- exercise/BOJ2667.java
+++ exercise/BOJ2667.java
@@ -1,25 +1,63 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-
 /**
  * BOJ2667
  */
 public class BOJ2667 {
-
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
     private static int[][] graph;
+    private static int[][] visited;
     private static int n;
+    static int cnt = 0;
+    static ArrayList<Integer> a = new ArrayList<Integer>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         graph = new int[n][n];
+        visited = new int[n][n];
+
         for (int i = 0; i < n; i++){
             String input = sc.next();
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < n; j++){
                 graph[i][j] = input.charAt(j) - '0';
             }
         }
 
-        System.out.println(Arrays.deepToString(graph));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if(graph[i][j] == 1 && visited[i][j] == 0){
+                    cnt = 1;
+                    visited[i][j] = 1;
+                    solution(i,j);
+                    a.add(cnt);
+                }
+            }
+        }
 
+        Collections.sort(a);
+        System.out.println(a.size());
+        for(int i : a){
+            System.out.println(i);
+        }
+       
+    }
+
+    public static void solution(int col,int row){
+
+        for (int i = 0; i < 4; i++){
+            int moveY = col + dy[i];
+            int moveX = row + dx[i];
+
+            if(0 <= moveX && moveX < n && moveY < n && 0 <= moveY){
+                if(graph[moveY][moveX] == 1 && visited[moveY][moveX] != 1 ){
+                    cnt++;
+                    visited[moveY][moveX] = 1;
+                    solution(moveY,moveX);
+                }
+            }
+
+        }
     }
 }
