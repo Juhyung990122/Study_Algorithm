f60ff7fcb7921de7adf0ac023594d2e2d6d1b4b5
Juhyung990122 <harukawa99@naver.com>
2021-09-12 19:03:31 +0900

    [Programmers]더 맵게 파이썬

--- exercise/프린터.java
+++ exercise/프린터.java
@@ -1,3 +1,4 @@
+import javax.print.Doc;
 import java.io.*;
 import java.util.*;
 
@@ -9,31 +10,29 @@ public class 프린터 {
     public static int solution(int[] priorities, int location) {
         int answer = 0;
         Queue<Document> queue = new LinkedList<>();
-
-        for( int i = 0; i < priorities.length; i++) {
-            queue.offer(new Document(i,priorities[i]));
+        for (int i = 0; i < priorities.length; i++){
+            queue.add(new Document(i,priorities[i]));
         }
 
         while(!queue.isEmpty()){
-            boolean flag = false;
-            int peek = queue.peek().priority;
+            boolean check = false;
+            int first = queue.peek().priority;
             for(Document document : queue){
-                //뺀거보다 더 높은게 있는지 검색한 뒤
-                if(peek < document.priority){
-                    //있으면 뺸건 뒤로 보낸다
-                   flag = true;
+                if(document.priority > first){
+                    check = true;
                 }
             }
 
-                if(flag){
-                    queue.offer(queue.poll());
-                }
-                else{
-                    if(queue.poll().location == location){
-                        answer = priorities.length - queue.size();
-                    }
+            if(check){
+                queue.offer(queue.poll());
+            }
+            else{
+                if(queue.poll().location == location){
+                    answer = priorities.length - queue.size();
                 }
             }
+        }
+
         return answer;
     }
 
