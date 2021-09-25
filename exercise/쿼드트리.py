3f0d32e0da00c967eec63c04adad3bbe5c6417ce
Juhyung990122 <harukawa99@naver.com>
2021-09-25 09:48:52 +0900

    [baekjoon]컨베이어 벨트 위의 로봇

--- exercise/쿼드트리.py
+++ exercise/쿼드트리.py
@@ -3,30 +3,26 @@ graph = list()
 for i in range(n):
     graph.append(list(map(int,input())))
 
-def divide(x,y,n):
+def qt(x,y,n):
     if n == 1:
-        print(graph[y][x],end = "")
+        print(graph[y][x],end="")
         return
-    # 같다면 트루
-    flag = True
-
+    same = True
     for i in range(y,y+n):
         for j in range(x,x+n):
             if graph[i][j] != graph[y][x]:
-                flag = False
+                same = False
                 break
-        
-    if flag:
-        print(graph[y][x], end= "")
+    
+    if same == True:
+        print(graph[y][x],end="")
     else:
-        d_n = n // 2
-
-        print("(", end = "")
-        divide(x,y,d_n)
-        divide(x+d_n,y,d_n)
-        divide(x,y+d_n,d_n)
-        divide(x+d_n,y+d_n,d_n)
+        print("(",end="")
+        n //= 2
+        qt(x,y,n)
+        qt(x+n,y,n)
+        qt(x,y+n,n)
+        qt(x+n,y+n,n)
         print(")",end="")
 
-divide(0,0,n)
-
+qt(0,0,n)
