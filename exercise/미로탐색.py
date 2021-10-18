f40464576190b6b752e1d1ec0435c91f77b106c8
Juhyung990122 <harukawa99@naver.com>
2021-10-18 21:14:14 +0900

    [Baekjoon] 미로탐색(re)

--- exercise/미로탐색.py
+++ exercise/미로탐색.py
@@ -1,31 +1,29 @@
 from collections import deque
 
 n,m = map(int,input().split())
-graph = []
-for _ in range(n):
+graph = list()
+for _ in range(n): 
     graph.append(list(map(int,input())))
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+visited = [[0] * m for i in range(n)]
+queue = deque()
+queue.append([0,0])
+visited[0][0] = 1
 
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            
-            if graph[nx][ny] == 0:
-                continue
-        
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
-    return graph[n-1][m-1]
+while queue:
+    y,x = queue.popleft()
+    if y == n-1 and x == m-1:
+        print(visited[n-1][m-1])
+        break
 
-print(bfs(0,0))
+    for i in range(4):
+        move_y = y + dy[i]
+        move_x = x + dx[i]
+        if 0 > move_y or move_y >= n or 0 > move_x or move_x >= m:
+            continue
+        if visited[move_y][move_x] == 0 and graph[move_y][move_x] == 1:
+            queue.append([move_y,move_x])
+            visited[move_y][move_x] = visited[y][x] + 1
